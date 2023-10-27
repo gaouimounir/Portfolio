d0dc0f1bb4d2339e919ee93e97715d2430146a78
gaouimounir <mounir.gaoui@outlook.com>
2023-10-27 16:43:49 +0200

    premier rendu

--- script.js
+++ script.js
@@ -9,3 +9,42 @@ navLinks.forEach((link) => {
     }
   });
 });
+
+const contactForm = document.getElementById("contact-form");
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const formData = new FormData(contactForm);
+
+  // Vous pouvez envoyer ces données à votre serveur pour les traiter ici
+  // Par exemple, en utilisant fetch() pour faire une requête POST
+
+  // Après l'envoi, vous pouvez réinitialiser le formulaire ou afficher un message de confirmation
+  contactForm.reset();
+  alert("Message envoyé avec succès.");
+});
+
+const projectCarousel = document.querySelector(".project-carousel");
+const projects = projectCarousel.querySelectorAll(".project");
+let currentProject = 0;
+
+function showProject(index) {
+  projects.forEach((project, i) => {
+    project.style.display = i === index ? "block" : "none";
+  });
+}
+
+// Initial display
+showProject(currentProject);
+
+// Previous project
+document.getElementById("prev-project").addEventListener("click", () => {
+  currentProject = (currentProject - 1 + projects.length) % projects.length;
+  showProject(currentProject);
+});
+
+// Next project
+document.getElementById("next-project").addEventListener("click", () => {
+  currentProject = (currentProject + 1) % projects.length;
+  showProject(currentProject);
+});
