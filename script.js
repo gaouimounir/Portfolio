539717fa676321ab70ca893a7ccc342538fb84e3
gaouimounir <mounir.gaoui@outlook.com>
2023-10-27 16:22:09 +0200

    premier exemple

---
+++ script.js
@@ -0,0 +1,11 @@
+const navLinks = document.querySelectorAll("nav a");
+navLinks.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    const targetId = link.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
