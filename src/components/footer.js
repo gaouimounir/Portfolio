8d46b2a5f063535f558fced089d684c6a77b4a0c
gaouimounir <mounir.gaoui@outlook.com>
2023-11-01 20:35:29 +0100

    header and footer style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,14 @@
+import React from "react";
+import "../styles/footer.css";
+
 const RenderFooter = () => {
-  return <p>&copy; 2023 Gaoui Mounir</p>;
+  return (
+    <div className="footer">
+      <p>Tous droits réservés</p>
+      <p>Contact</p>
+      <p>&copy; 2023 Gaoui Mounir</p>
+    </div>
+  );
 };
 
 export default RenderFooter;
