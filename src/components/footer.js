ebd18490a97a2af849e281a343f9c72a515ee357
gaouimounir <mounir.gaoui@outlook.com>
2023-11-02 19:25:03 +0100

    canceled header and footer style

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,33 +1,13 @@
 import React from "react";
-import styled from "styled-components";
-
-const FooterContainer = styled.footer`
-  background-color: #333;
-  color: #fff;
-  padding: 20px 0;
-  display: flex;
-  justify-content: space-between;
-  transition: background-color 0.3s;
-`;
-
-const FooterItem = styled.p`
-  margin: 0 20px;
-  font-size: 18px;
-  cursor: pointer;
-  transition: color 0.3s;
-
-  &:hover {
-    color: #007bff;
-  }
-`;
+import "../styles/footer.css";
 
 const RenderFooter = () => {
   return (
-    <FooterContainer>
-      <FooterItem>Tous droits réservés</FooterItem>
-      <FooterItem>Contact</FooterItem>
-      <FooterItem>&copy; 2023 Gaoui Mounir</FooterItem>
-    </FooterContainer>
+    <div className="footer">
+      <p>Tous droits réservés</p>
+      <p>Contact</p>
+      <p>&copy; 2023 Gaoui Mounir</p>
+    </div>
   );
 };
 
