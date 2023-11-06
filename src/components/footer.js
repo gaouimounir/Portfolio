6c96e1aa9dee584af1f9c4e01ab1b64b16f87ac8
gaouimounir <mounir.gaoui@outlook.com>
2023-11-06 16:47:35 +0100

    link in icon contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,9 +6,15 @@ const RenderFooter = () => {
     <div className="footer">
       <p>&copy; 2023 Gaoui Mounir</p>
       <div className="contact">
-        <i class="fa-brands fa-linkedin"></i>
-        <i class="fa-brands fa-github"></i>
-        <i class="fa-solid fa-envelope"></i>
+        <a href="https://www.linkedin.com/in/am-gaoui/">
+          <i class="fa-brands fa-linkedin"></i>
+        </a>
+        <a href="mailto:am.gaoui@protonmail.com">
+          <i class="fa-solid fa-envelope"></i>
+        </a>
+        <a href="https://github.com/gaouimounir">
+          <i class="fa-brands fa-github"></i>
+        </a>
       </div>
       <p>Tous droits réservés</p>
     </div>
