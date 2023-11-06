8c823262c905c0ddbd54f7e0e857373bd3d2ff25
gaouimounir <mounir.gaoui@outlook.com>
2023-11-06 16:04:50 +0100

    add icon contact

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,7 +5,11 @@ const RenderFooter = () => {
   return (
     <div className="footer">
       <p>Tous droits réservés</p>
-      <p>Contact</p>
+      <div className="contact">
+        Contact :<i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-github"></i>
+        <i class="fa-solid fa-envelope"></i>
+      </div>
       <p>&copy; 2023 Gaoui Mounir</p>
     </div>
   );
