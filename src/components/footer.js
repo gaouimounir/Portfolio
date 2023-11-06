3678e0727b8b874680eedc6baa12b4a7ee023bbf
gaouimounir <mounir.gaoui@outlook.com>
2023-11-06 16:26:57 +0100

    new style css

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,13 +4,13 @@ import "../styles/footer.css";
 const RenderFooter = () => {
   return (
     <div className="footer">
-      <p>Tous droits réservés</p>
+      <p>&copy; 2023 Gaoui Mounir</p>
       <div className="contact">
-        Contact :<i class="fa-brands fa-linkedin"></i>
+        <i class="fa-brands fa-linkedin"></i>
         <i class="fa-brands fa-github"></i>
         <i class="fa-solid fa-envelope"></i>
       </div>
-      <p>&copy; 2023 Gaoui Mounir</p>
+      <p>Tous droits réservés</p>
     </div>
   );
 };
