67d104d7ab184e6b619743888ef6953ec3ddddc5
gaouimounir <mounir.gaoui@outlook.com>
2023-11-08 15:12:27 +0100

    start rooter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import "./App.css";
 import Welcome from "./components/Welcome";
+import Projects from "./components/Projets";
 import RenderFooter from "./components/footer";
 import RenderHeader from "./components/header";
+import {
+  NavLink,
+  Outlet,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+
+function Root() {
+  return (
+    <>
+      <header>
+        <nav></nav>
+      </header>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        <Root />
+      </div>
+    ),
+    children: [
+      {
+        path: ":category",
+        element: <Projects />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <>
       <RenderHeader />
+      <RouterProvider router={router} />
       <Welcome />
       <RenderFooter />
     </>
