7bf0426dc52116c29528e1254885af9f97245394
gaouimounir <mounir.gaoui@outlook.com>
2023-11-10 17:41:27 +0100

    animation text

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "../styles/welcome.css";
 
+const text = "Texte à défiler lettre par lettre.";
+const container = document.getElementById("scrolling-text");
+
+function displayText(index) {
+  if (index <= text.length) {
+    container.innerHTML = text.substring(0, index);
+    index++;
+    setTimeout(() => displayText(index), 100); // ajustez la durée ici
+  }
+}
+
+displayText(0);
+
 function Welcome() {
   return (
     <div className="welcome">
