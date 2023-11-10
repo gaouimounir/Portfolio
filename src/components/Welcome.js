29ca031441bd4de68e8e1dd5ab8c7be89ffa06df
gaouimounir <mounir.gaoui@outlook.com>
2023-11-10 17:50:14 +0100

    animation texte defillant

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,23 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/welcome.css";
 
-const text = "Texte à défiler lettre par lettre.";
-const container = document.getElementById("scrolling-text");
-
-function displayText(index) {
-  if (index <= text.length) {
-    container.innerHTML = text.substring(0, index);
-    index++;
-    setTimeout(() => displayText(index), 100); // ajustez la durée ici
-  }
-}
-
-displayText(0);
-
 function Welcome() {
+  const [displayedText, setDisplayedText] = useState("");
+
+  useEffect(() => {
+    const originalText = "Hi, I'm Mounir Gaoui";
+    let index = 0;
+
+    const intervalId = setInterval(() => {
+      if (index <= originalText.length) {
+        setDisplayedText(originalText.substring(0, index));
+        index++;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, 100); // ajustez la durée ici
+
+    return () => clearInterval(intervalId);
+  }, []); // le tableau vide [] assure que useEffect s'exécute une seule fois après le montage du composant
+
   return (
     <div className="welcome">
-      <h1>Hi, I'm Mounir Gaoui</h1>
+      <h1>{displayedText}</h1>
       <h2>A Front-End Developer</h2>
       <p>I'm passionate about creating beautiful and user-friendly websites.</p>
     </div>
