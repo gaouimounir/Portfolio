805793fc82c218d948ba146093192e1fb10754d8
gaouimounir <mounir.gaoui@outlook.com>
2023-11-13 10:38:01 +0100

    arriere plan

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -2,42 +2,53 @@ import React, { useEffect, useState } from "react";
 import "../styles/welcome.css";
 
 function Welcome() {
+  // State pour stocker le texte à afficher
   const [displayedText, setDisplayedText] = useState("");
 
   useEffect(() => {
+    // Texte original à dérouler
     const originalText = "Hi, I'm Mounir Gaoui";
     let index = 0;
-    let intervalId;
+    let intervalId; // Variable pour stocker l'ID de l'intervalle
 
+    // Fonction pour démarrer la boucle de défilement
     const startLoop = () => {
       intervalId = setInterval(() => {
         if (index <= originalText.length) {
+          // Met à jour le texte affiché à chaque itération
           setDisplayedText(originalText.substring(0, index));
           index++;
         } else {
-          clearInterval(intervalId);
+          clearInterval(intervalId); // Arrête l'intervalle lorsque tout le texte est affiché
+
+          // Défini un délai avant de réinitialiser le texte et redémarrer la boucle
           setTimeout(() => {
-            setDisplayedText("");
-            index = 0;
-            startLoop();
-          }, 1000);
+            setDisplayedText(""); // Réinitialise le texte
+            index = 0; // Réinitialise l'index
+            startLoop(); // Redémarre la boucle
+          }, 1000); // Délai avant la prochaine itération (ajustez au besoin)
         }
-      }, 100);
+      }, 100); // Ajustez la durée de l'intervalle ici
     };
 
-    startLoop();
+    startLoop(); // Appelle la fonction pour démarrer la boucle au montage du composant
 
     // Nettoie l'intervalle lors du démontage du composant
     return () => {
       clearInterval(intervalId);
     };
-  }, []);
+  }, []); // Le tableau vide [] assure que useEffect s'exécute une seule fois après le montage du composant
 
+  // Rendu du composant avec le texte en cours de défilement
   return (
     <div className="welcome">
-      <h1>{displayedText}</h1>
-      <h2>A Front-End Developer</h2>
-      <p>I'm passionate about creating beautiful and user-friendly websites.</p>
+      <div className="text">
+        <h1>{displayedText}</h1>
+        <h2>A Front-End Developer</h2>
+        <p>
+          I'm passionate about creating beautiful and user-friendly websites.
+        </p>
+      </div>
     </div>
   );
 }
