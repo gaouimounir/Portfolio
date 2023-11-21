565fc62191f42a8db8cc40e93d51f9d415ee29e6
gaouimounir <mounir.gaoui@outlook.com>
2023-11-21 20:52:05 +0100

    add card

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -46,6 +46,15 @@ function Welcome() {
         <div className="text">
           <p>{displayedText}</p>
         </div>
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
