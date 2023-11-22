075f38a61717336e1912c614864257cf3815222f
gaouimounir <mounir.gaoui@outlook.com>
2023-11-22 17:20:13 +0100

    supression style css header footer

--- src/App.js
+++ src/App.js
@@ -23,6 +23,21 @@ function Root() {
       <div>
         <Outlet />
       </div>
+      <div className="footer">
+        <p>&copy; 2023 Gaoui Mounir</p>
+        <div className="contact">
+          <a href="https://www.linkedin.com/in/am-gaoui/">
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+          <a href="mailto:am.gaoui@protonmail.com">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+          <a href="https://github.com/gaouimounir">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
+        <p>Tous droits réservés</p>
+      </div>
     </>
   );
 }
--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../styles/header.css";
 
 const RenderHeader = () => {
   return (
